33638c85a8b43dbf21fddb93e3a7899033e3ede5
Tommy Bishop <bishop.thomas@gmail.com>
2018-04-27 21:41:43 -0700

    update alphanumeric validation in api

--- api/handlers.go
+++ api/handlers.go
@@ -59,7 +59,7 @@ type clientErrorResponse struct {
 }
 
 func validateAlphanumericString(s string) (validation, error) {
-	matched, err := regexp.MatchString("^[A-z-]+$", s)
+	matched, err := regexp.MatchString("^[A-z0-9-]+$", s)
 	if err != nil {
 		log.Printf("Unable to validate alphanumeric string; err: %s\n", err)
 		return validation{}, err
