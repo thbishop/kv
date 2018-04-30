7e4b95433c53df5cb18131e71963b0ad068a6399
Tommy Bishop <bishop.thomas@gmail.com>
2018-04-30 01:01:50 -0700

    refactored cli and added tests

--- cli/client/main.go
+++ cli/client/main.go
@@ -12,51 +12,34 @@ import (
 
 var apiURL = "https://kv-api.dyson-sphere.com/"
 
-func storeURL(storeName string) string {
-	return apiURL + "/stores/" + storeName
-}
-
-func keyURL(storeName string, keyName string) string {
-	return storeURL(storeName) + "/keys/" + keyName
-}
-
-func makeRequest(method string, url string, body io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest(method, url, body)
-	if err != nil {
-		return &http.Response{}, err
-	}
-
-	client := &http.Client{}
-	return client.Do(req)
-}
-
-func simpleRequestWrapper(method string, url string, body io.Reader) error {
-	resp, err := makeRequest("PUT", url, body)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		return nil
-	}
-
-	return errorFromResponse(resp)
+var prettyServerError = map[int]string{
+	500: "internal server error",
+	501: "not implemented",
+	502: "bad gateway",
+	503: "service unavailable",
+	504: "gateway timeout",
+	505: "HTTP version not support",
+	506: "variant also negotiates",
+	507: "insufficient storage",
+	508: "loop detected",
+	510: "not extended",
+	511: "network authentication required",
 }
 
 func CreateStore(storeName string) error {
-	return simpleRequestWrapper("PUT", storeURL(storeName), nil)
+	return simpleRequestWrapper(makeRequest("PUT", storeURL(storeName), nil))
 }
 
 func SetKey(storeName string, keyName string, keyValue string) error {
-	return simpleRequestWrapper("PUT", keyURL(storeName, keyName), strings.NewReader(keyValue))
+	return simpleRequestWrapper(makeRequest("PUT", keyURL(storeName, keyName), strings.NewReader(keyValue)))
 }
 
 func DeleteStore(storeName string) error {
-	return simpleRequestWrapper("DELETE", storeURL(storeName), nil)
+	return simpleRequestWrapper(makeRequest("DELETE", storeURL(storeName), nil))
 }
 
 func DeleteKey(storeName string, keyName string) error {
-	return simpleRequestWrapper("DELETE", keyURL(storeName, keyName), nil)
+	return simpleRequestWrapper(makeRequest("DELETE", keyURL(storeName, keyName), nil))
 }
 
 func GetKey(storeName string, keyName string) ([]byte, error) {
@@ -88,7 +71,7 @@ type apiError struct {
 
 func errorFromResponse(resp *http.Response) error {
 	if resp.StatusCode >= 500 {
-		return errors.New(fmt.Sprintf("server error (%s)", prettyServerError(resp.StatusCode)))
+		return errors.New(fmt.Sprintf("server error (%s)", prettyServerError[resp.StatusCode]))
 	}
 
 	if resp.StatusCode >= 400 && resp.StatusCode < 500 {
@@ -101,11 +84,11 @@ func errorFromResponse(resp *http.Response) error {
 
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			return errors.New(fmt.Sprintf("unknown error (response code %s)", resp.StatusCode))
+			return errors.New(fmt.Sprintf("unknown error (response code %d)", resp.StatusCode))
 		}
 		err = json.Unmarshal(body, &apiErr)
 		if err != nil {
-			return errors.New(fmt.Sprintf("unknown error (response code %s)", resp.StatusCode))
+			return errors.New(fmt.Sprintf("unknown error (response code %d)", resp.StatusCode))
 		}
 
 		return errors.New(apiErr.Error)
@@ -114,20 +97,32 @@ func errorFromResponse(resp *http.Response) error {
 	return errors.New(fmt.Sprintf("unknown error (%v)", resp))
 }
 
-func prettyServerError(code int) string {
-	codes := map[int]string{
-		500: "internal server error",
-		501: "not implemented",
-		502: "bad gateway",
-		503: "service unavailable",
-		504: "gateway timeout",
-		505: "HTTP version not support",
-		506: "variant also negotiates",
-		507: "insufficient storage",
-		508: "loop detected",
-		510: "not extended",
-		511: "network authentication required",
+func storeURL(storeName string) string {
+	return apiURL + "/stores/" + storeName
+}
+
+func keyURL(storeName string, keyName string) string {
+	return storeURL(storeName) + "/keys/" + keyName
+}
+
+func simpleRequestWrapper(resp *http.Response, err error) error {
+	if err != nil {
+		return err
 	}
 
-	return codes[code]
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		return nil
+	}
+
+	return errorFromResponse(resp)
+}
+
+func makeRequest(method string, url string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return &http.Response{}, err
+	}
+
+	client := &http.Client{}
+	return client.Do(req)
 }
---
+++ cli/client/main_test.go
@@ -0,0 +1,132 @@
+package client
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKeyURL(t *testing.T) {
+	expected := apiURL + "/stores/foo/keys/bar"
+	assert.Equal(t, expected, keyURL("foo", "bar"))
+}
+
+func TestStoreURL(t *testing.T) {
+	expected := apiURL + "/stores/foo"
+	assert.Equal(t, expected, storeURL("foo"))
+}
+
+func TestIsNotFoundError(t *testing.T) {
+	assert.Equal(t, true, IsNotFoundError(errors.New("not found")))
+	assert.Equal(t, false, IsNotFoundError(errors.New("blah")))
+}
+
+func TestErrorFromResponse(t *testing.T) {
+	var tests = []struct {
+		response    *http.Response
+		expectedErr error
+	}{
+		{
+			response:    &http.Response{StatusCode: 500},
+			expectedErr: errors.New("server error (internal server error)"),
+		},
+		{
+			response:    &http.Response{StatusCode: 501},
+			expectedErr: errors.New("server error (not implemented)"),
+		},
+		{
+			response: &http.Response{
+				StatusCode: 400,
+				Body:       ioutil.NopCloser(strings.NewReader(`{"error": "this is an api error"}`)),
+			},
+			expectedErr: errors.New("this is an api error"),
+		},
+		{
+			response: &http.Response{
+				StatusCode: 400,
+				Body:       ioutil.NopCloser(strings.NewReader("blah blah")),
+			},
+			expectedErr: errors.New("unknown error (response code 400)"),
+		},
+		{
+			response:    &http.Response{StatusCode: 404},
+			expectedErr: errors.New("not found"),
+		},
+		{
+			response:    &http.Response{StatusCode: 100},
+			expectedErr: errors.New(fmt.Sprintf("unknown error (%v)", &http.Response{StatusCode: 100})),
+		},
+	}
+
+	for _, tc := range tests {
+		assert.Equal(t, tc.expectedErr, errorFromResponse(tc.response))
+	}
+
+}
+
+func TestSimpleRequestWrapper(t *testing.T) {
+	var tests = []struct {
+		response      *http.Response
+		err           error
+		errorExpected bool
+	}{
+		{
+			response:      &http.Response{StatusCode: 200},
+			err:           nil,
+			errorExpected: false,
+		},
+		{
+			response:      &http.Response{StatusCode: 200},
+			err:           errors.New(""),
+			errorExpected: true,
+		},
+		{
+			response:      &http.Response{StatusCode: 201},
+			err:           nil,
+			errorExpected: false,
+		},
+		{
+			response:      &http.Response{StatusCode: 201},
+			err:           errors.New(""),
+			errorExpected: true,
+		},
+		{
+			response: &http.Response{
+				StatusCode: 400,
+				Body:       ioutil.NopCloser(strings.NewReader(`{"error": "this is an api error"}`)),
+			},
+			err:           nil,
+			errorExpected: true,
+		},
+		{
+			response: &http.Response{
+				StatusCode: 400,
+				Body:       ioutil.NopCloser(strings.NewReader(`{"error": "this is an api error"}`)),
+			},
+			err:           errors.New(""),
+			errorExpected: true,
+		},
+		{
+			response:      &http.Response{StatusCode: 500},
+			errorExpected: true,
+		},
+		{
+			response:      &http.Response{StatusCode: 500},
+			err:           errors.New(""),
+			errorExpected: true,
+		},
+	}
+
+	for _, tc := range tests {
+		if tc.errorExpected {
+			assert.Error(t, simpleRequestWrapper(tc.response, tc.err))
+		} else {
+			assert.NoError(t, simpleRequestWrapper(tc.response, tc.err))
+		}
+	}
+}
